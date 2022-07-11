d3719d3ed035a7590263c711d30e6aff997b9e84
LeonidGHA <stowk1992@gmail.com>
2022-07-11 22:44:55 +0300

    add change 1 and 2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,52 +10,20 @@ const mainGalleryEl = document.querySelector('.gallery');
 const imagesCardEl = createImagesCards(galleryItems);
 
 mainGalleryEl.insertAdjacentHTML('beforeend', imagesCardEl);
-mainGalleryEl.addEventListener('click', onMainGalleryElClick);
 
 function createImagesCards(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-            />
-        </a>
-    </div>
-      `;
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+    `;
     })
     .join('');
 }
 
-let instance;
-
-function onMainGalleryElClick(evt) {
-  evt.preventDefault();
-
-  if (evt.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  instance = basicLightbox.create(`
-    <img src="${evt.target.dataset.source}" width="800" height="600">
-`);
-
-  instance.show(window.addEventListener('keydown', onEscBtnPush));
-}
-
-const closeModalWindow = event => {
-  instance.close();
-  window.removeEventListener('keydown', onEscBtnPush);
-};
-
-const onEscBtnPush = event => {
-  if (event.code !== 'Escape') {
-    return;
-  }
-
-  closeModalWindow();
-};
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'current-time';
+
+player.on('timeupdate', function (data) {
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(data));
+  //   console.log(data);
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {};
+
+player.on('play', onPlay);
